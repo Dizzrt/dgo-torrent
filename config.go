292512364b6939ee8ccb206c6d2bada4843a8961
Dizzrt <dizzrt.s@qq.com>
2023-10-23 20:07:40 +0800

    add config

---
+++ config.go
@@ -0,0 +1,87 @@
+package dgotorrent
+
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"os"
+
+	"github.com/spf13/viper"
+)
+
+type DConfig struct{}
+
+var (
+	dconfig *DConfig
+	config  *viper.Viper
+)
+
+const (
+	_CONFIG_PATH = "./.dgo_torrent.toml"
+
+	KEY_PEER_ID = "client.peer_id"
+)
+
+func init() {
+	// check config file
+	_, err := os.Stat(_CONFIG_PATH)
+	if err != nil {
+		if os.IsNotExist(err) {
+			_, err = os.Create(_CONFIG_PATH)
+			if err != nil {
+				panic(fmt.Sprintf("[dgo_torrent] create config file failed with error: %v\n", err))
+			}
+		} else {
+			panic(fmt.Sprintf("[dgo_torrent] check config file failed with error: %v\n", err))
+		}
+	}
+
+	// init viper
+	config = viper.New()
+	config.SetConfigFile(_CONFIG_PATH)
+
+	err = config.ReadInConfig()
+	if err != nil {
+		panic(fmt.Sprintf("[dgo_torrent] viper read config failed with error: %v", err))
+	}
+}
+
+func generatePeerID() string {
+	charSet := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	charSetLength := big.NewInt(int64(len(charSet)))
+
+	randomString := make([]byte, 14)
+	for i := 0; i < 14; i++ {
+		index, err := rand.Int(rand.Reader, charSetLength)
+		if err != nil {
+			panic(err)
+		}
+		randomString[i] = charSet[index.Int64()]
+	}
+
+	return fmt.Sprintf("DT-%s-%s", "00", string(randomString))
+}
+
+func Config() *DConfig {
+	return dconfig
+}
+
+func (cf *DConfig) GetConfig() *viper.Viper {
+	return config
+}
+
+func (cf *DConfig) GetPeerID() string {
+	// if !config.IsSet(KEY_PEER_ID) {
+	// 	config.Set(KEY_PEER_ID, generatePeerID())
+	// 	config.WriteConfig()
+	// }
+
+	// id := config.GetString(KEY_PEER_ID)
+
+	// idBytes:= []byte(id)
+	// if _,ok:= idBytes.([PEER_ID_LEN]byte); !ok {
+
+	// }
+
+	return config.GetString(KEY_PEER_ID)
+}
